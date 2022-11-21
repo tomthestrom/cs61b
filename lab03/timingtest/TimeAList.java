ecc67049424e972dc9d4d3719a41376801cd20b2
tomthestrom <thruska29@gmail.com>
2022-11-21 10:06:39 +0100

    AList timeAListConstruction method done, Alist-bad-resize.txt output with timeAlistConstruction results (as expected)

--- lab03/timingtest/TimeAList.java
+++ lab03/timingtest/TimeAList.java
@@ -1,5 +1,6 @@
 package timingtest;
 import edu.princeton.cs.algs4.Stopwatch;
+import org.junit.Test;
 
 /**
  * Created by hug.
@@ -21,7 +22,41 @@ public class TimeAList {
         timeAListConstruction();
     }
 
+
+    /**
+     * Returns a table with rows at N breakpoints from startingN to MAX_N for addLast operation of AList
+     */
     public static void timeAListConstruction() {
-        // TODO: YOUR CODE HERE
+        //starting nr of ops
+        int startingN = Math.multiplyExact(1, 1000);
+        //max nr of ops
+        int MAX_N = Math.multiplyExact(128, 1000);
+
+        AList Ns = new AList<Integer>();
+        AList times = new AList<Double>();
+        AList opCounts = new AList<Integer>();
+        /**
+         * for MAX_N = 128 runs 8 times - br points:
+         * 1 000 / 2 000/ 4 000/ 8 000/ 16 000/ 32 000 /64 000 /128 000
+         */
+        for (int n = startingN; n <= MAX_N; n *=  2) {
+            AList timedA = new AList<Integer>();
+            Stopwatch sw = new Stopwatch();
+
+            int opCount;
+            /**
+             * Fill ArrayList timedA with ints from 0 to N
+             * */
+            for (opCount = 0; opCount < n; opCount += 1) {
+                timedA.addLast(opCount);
+            }
+            //stop Stopwatch
+            times.addLast(sw.elapsedTime());
+
+            Ns.addLast(n);
+            opCounts.addLast(opCount);
+        }
+
+        printTimingTable(Ns, times, opCounts);
     }
 }
