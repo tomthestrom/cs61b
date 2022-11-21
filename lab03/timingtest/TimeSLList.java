4524d36b03020e11404f0e2bd1de3c4685cf7d07
tomthestrom <thruska29@gmail.com>
2022-11-21 10:40:48 +0100

    TimeSLList timeGetLast() method done, SLList-getLast.txt - method's output with results as expected

--- lab03/timingtest/TimeSLList.java
+++ lab03/timingtest/TimeSLList.java
@@ -21,8 +21,53 @@ public class TimeSLList {
         timeGetLast();
     }
 
+    /**
+     * Returns a table with rows at N breakpoints from startingN to MAX_N for getLast operation of SLList
+     * Constant amount of operations specified by MAX_OP_COUNT
+     */
     public static void timeGetLast() {
-        // TODO: YOUR CODE HERE
+        //starting nr of ops
+        int startingN = Math.multiplyExact(1, 1000);
+        //max nr of ops
+        int MAX_N = Math.multiplyExact(128, 1000);
+
+        int MAX_OP_COUNT = Math.multiplyExact(1, 10000);
+
+        AList Ns = new AList<Integer>();
+        AList times = new AList<Double>();
+        AList opCounts = new AList<Integer>();
+
+        /**
+         * for MAX_N = 128 runs 8 times - br points:
+         * 1 000 / 2 000/ 4 000/ 8 000/ 16 000/ 32 000 /64 000 /128 000
+         * Fills SLList with N items
+         * Calls getLast() of the SLList MAX_OP_COUNT times
+         */
+        for (int n = startingN; n <= MAX_N; n *=  2) {
+            SLList timedSLL = new SLList<Integer>();
+
+            int opCount;
+
+            /**
+             * Fill SLList timedSLL with stuffing (ints from 0 to N)
+             * */
+            for (int stuffing = 0; stuffing < n; stuffing += 1) {
+                timedSLL.addLast(stuffing);
+            }
+
+            Stopwatch sw = new Stopwatch();
+            // call getLast() MAX_OP_COUNT times
+            for (opCount = 0; opCount < MAX_OP_COUNT; opCount += 1) {
+                timedSLL.getLast();
+            }
+            //stop Stopwatch
+            times.addLast(sw.elapsedTime());
+
+            Ns.addLast(n);
+            opCounts.addLast(opCount);
+        }
+
+        printTimingTable(Ns, times, opCounts);
     }
 
 }
