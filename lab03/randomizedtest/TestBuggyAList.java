969634e79ba65bde00180495b0054f7d994ff9ad
tomthestrom <thruska29@gmail.com>
2022-11-21 14:33:16 +0100

    TestBuggyAList - randomizedTest() working according to spec, getLast, removeLast operations added, comparison of AListNoResizing and BuggyAList added, if/else tossed out in favor of switch for operationNumber,  HashMap to keep track of sizeNonZero ops in constructor - check before executing an operation by looking up the value for the operationNumber

--- lab03/randomizedtest/TestBuggyAList.java
+++ lab03/randomizedtest/TestBuggyAList.java
@@ -4,6 +4,8 @@ import edu.princeton.cs.algs4.StdRandom;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.HashMap;
+
 import static org.junit.Assert.*;
 
 /**
@@ -11,6 +13,20 @@ import static org.junit.Assert.*;
  */
 public class TestBuggyAList {
     // YOUR TESTS HERE
+    private HashMap<Integer, Boolean> SizeNonZeroOperation;
+    public TestBuggyAList() {
+        //some operations can't be executed if AList.size() == 0
+        SizeNonZeroOperation = new HashMap<>();
+
+        // addLast
+        SizeNonZeroOperation.put(0, false);
+        // size
+        SizeNonZeroOperation.put(1, false);
+        // getLast
+        SizeNonZeroOperation.put(2, true);
+        // removeLast
+        SizeNonZeroOperation.put(3, true);
+    }
 
     /*
      * Simple comparison test
@@ -40,22 +56,55 @@ public class TestBuggyAList {
         assertEquals(ALNR.removeLast(), BAL.removeLast());
     }
 
+
     @Test
     public void randomizedTest() {
         AListNoResizing<Integer> L = new AListNoResizing<>();
+        BuggyAList<Integer> BL = new BuggyAList<>();
+
 
         int N = 500;
         for (int i = 0; i < N; i += 1) {
-            int operationNumber = StdRandom.uniform(0, 2);
-            if (operationNumber == 0) {
-                // addLast
-                int randVal = StdRandom.uniform(0, 100);
-                L.addLast(randVal);
-                System.out.println("addLast(" + randVal + ")");
-            } else if (operationNumber == 1) {
-                // size
-                int size = L.size();
-                System.out.println("size: " + size);
+            int operationNumber = StdRandom.uniform(0, 4);
+
+            // if operation can't be executed proceed to the next iteration
+            if (L.size() == 0 && SizeNonZeroOperation.get(operationNumber)) {
+                continue;
+            }
+
+            switch (operationNumber) {
+                case 0 -> {
+                    int randVal = StdRandom.uniform(0, 100);
+                    L.addLast(randVal);
+                    BL.addLast(randVal);
+                    System.out.println("L, BL, addLast(" + randVal + ")");
+                }
+                case 1 -> {
+                    int sizeL = L.size();
+                    int sizeBL = BL.size();
+                    System.out.println("L.size: " + sizeL);
+                    System.out.println("BL.size: " + sizeBL);
+
+                    assertEquals(sizeL, sizeBL);
+                }
+                case 2 -> {
+                    int lastValL = L.getLast();
+                    int lastValBL = BL.getLast();
+
+                    System.out.println("L.getLast() -> " + lastValL);
+                    System.out.println("BL.getLast() -> " + lastValBL);
+
+                    assertEquals(lastValL, lastValBL);
+                }
+                case 3 -> {
+                    int lastValL = L.removeLast();
+                    int lastValBL = BL.removeLast();;
+                    System.out.println("L.removeLast() -> " + lastValL);
+                    System.out.println("BL.removeLast() -> " + lastValBL);
+
+                    assertEquals(lastValL, lastValBL);
+                }
+                default -> System.out.println("Operation nr " + operationNumber + " not implemented.");
             }
         }
     }
