b2417cdf2e179c334aa440ef99b0cff1019809c7
tomthestrom <thruska29@gmail.com>
2022-11-23 13:45:11 +0100

    ArrayDeque addFirst, addLast done and tested - reverse order experiment

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -16,11 +16,19 @@ public class ArrayDeque<T> {
         sizeLast = 0;
     }
 
+    /**
+     * Added item is always at the end of the array.
+     * @param item
+     */
     public void addFirst(T item) {
-        firstItems[firstItems.length - 1 - sizeFirst] = item;
+        firstItems[sizeFirst] = item;
         sizeFirst += 1;
     }
 
+    /**
+     * Added item is always at the current beginning
+     * @param item
+     */
     public void addLast(T item) {
         lastItems[lastItems.length - 1 - sizeLast] = item;
         sizeLast += 1;
@@ -40,11 +48,11 @@ public class ArrayDeque<T> {
 
     public T removeFirst() {
         T firstItem = firstItems[firstItems.length - sizeFirst];
-       firstItems[firstItems.length - sizeFirst] = null;
+        firstItems[firstItems.length - sizeFirst] = null;
 
-       sizeFirst -= 1;
+        sizeFirst -= 1;
 
-       return firstItem;
+        return firstItem;
     }
 
     public T removeLast() {
@@ -56,6 +64,15 @@ public class ArrayDeque<T> {
         return lastItem;
     }
 
+    /**
+     * Gets item by index
+     * Storing items is implemented as:
+        * firstItems = [ 4 , 3 , 2, 1] - latest first item at the end of the array
+        * lastItems = [8, 7, 6, 5] - latest added last item in the beginning of the array
+        * Conceptual array = [1, 2, 3, 4, 5, 6, 7, 8] - or reversed(firstItems) + reversed(lastItems)
+     * @param index
+     * @return
+     */
     public T get(int index) {
         if (index >= size()) {
             throw new IndexOutOfBoundsException();
@@ -64,9 +81,9 @@ public class ArrayDeque<T> {
         boolean isInFirstItems = index < sizeFirst;
 
         if (isInFirstItems) {
-            return  firstItems[index];
+            return firstItems[sizeFirst - 1 - index];
         } else {
-            return lastItems[sizeLast - index];
+            return lastItems[lastItems.length - 1 - index];
         }
     }
 }
--- proj1/deque/ArrayDequeTest.java
+++ proj1/deque/ArrayDequeTest.java
@@ -1,5 +1,95 @@
 package deque;
 
-public class ArrayDequeTest<T> {
+import org.junit.Test;
 
+import static org.junit.Assert.*;
+
+/* Performs ArrayDeque tests. */
+public class ArrayDequeTest<T> {
+    @Test
+    public void testAddFirstNoResizing() {
+        ArrayDeque<Integer> AD1 = new ArrayDeque<>();
+
+        AD1.addFirst(1);
+        AD1.addFirst(2);
+
+        int expected = 1;
+        int actual = AD1.get(1);
+
+        assertEquals(expected, actual);
+
+        AD1.addFirst(4);
+
+        expected = 4;
+        actual = AD1.get(0);
+        assertEquals(expected, actual);
+
+        AD1.addFirst(5);
+        expected = 4;
+        actual = AD1.get(3);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    /**
+     * After last call to addLast:
+     * Conceptual array: [1, 2, 4, 5]
+     * In underlying implementation: [5, 4, 2, 1]
+     */
+    public void testAddLastNoResizing() {
+        ArrayDeque<Integer> AD1 = new ArrayDeque<>();
+
+        AD1.addLast(1);
+        AD1.addLast(2);
+
+        int expected = 2;
+        int actual = AD1.get(1);
+
+        assertEquals(expected, actual);
+
+        AD1.addLast(4);
+
+        expected = 1;
+        actual = AD1.get(0);
+        assertEquals(expected, actual);
+
+        AD1.addLast(5);
+        expected = 5;
+        actual = AD1.get(3);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    /**
+     * After last add operation:
+     * Conceptual array: [0, 1, 2, 3]
+     * In underlying implementation:
+     * firstItems: [1, 0, null, null]
+     * lastItems: [null, null, 2, 3]
+     */
+    public void testGetNoResize() {
+        ArrayDeque<Integer> AD1 = new ArrayDeque<>();
+
+        AD1.addLast(3);
+        AD1.addLast(2);
+        AD1.addFirst(1);
+
+        int expected1 = 1;
+        int actual1 = AD1.get(0);
+
+        assertEquals(expected1, actual1);
+
+        AD1.addFirst(0);
+
+        int expected0 = 0;
+        int actual0 = AD1.get(0);
+
+        assertEquals(expected0, actual0);
+
+
+        int expected3 = 3;
+        int actual3 = AD1.get(3);
+
+        assertEquals(expected3, actual3);
+    }
 }
