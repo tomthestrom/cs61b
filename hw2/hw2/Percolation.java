b566f147d22b231406c5fcbbec0b0ad39bdde9bf
tomthestrom <thruska29@gmail.com>
2022-12-06 16:06:32 +0100

    hw2/Percolation class fields and constructor

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,48 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    /**
+     * 1-dimensional, holds N by N grid
+     * Row nr as index / N
+     * Col nr as index % N
+     */
+    private WeightedQuickUnionUF grid;
 
+    /**
+     * Mirrors grid
+     * Sets value at x, y to true, if site is opened
+     */
+    private boolean[] valGrid;
+    /**
+     * Nr of opened sites
+     */
+    private int openSitesNr;
+
+    /**
+     * The value of the virtual top Index
+     */
+    private int virtualTopIndex;
+
+    /**
+     * The value of the virtual bottom Index
+     */
+    private int virtualBottomIndex;
+
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException("N must be > 0");
+        }
+        int size = N * N;
+
+        //grid + virtual top - index: (size + 1) + virtual bottom - index: (size + 2)
+        grid = new WeightedQuickUnionUF(size + 2);
+        valGrid = new boolean[size];
+        virtualTopIndex = size - 2;
+        virtualBottomIndex = size - 1;
+        openSitesNr = 0;
+    }
+
+    public void open(int row, int col) {
+
+    }
 }
