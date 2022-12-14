f301fa98288089d9864d6b11aea9576bb1739b58
tomthestrom <thruska29@gmail.com>
2022-12-14 15:16:13 +0100

    lab/07/bstmap/BSTMap.java class added with scaffolding - overriding required methods as / spec

---
+++ lab07/bstmap/BSTMap.java
@@ -0,0 +1,59 @@
+package bstmap;
+
+import java.util.Iterator;
+import java.util.Set;
+
+public class BSTMap<K, V> implements Map61B<K, V> {
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        return false;
+    }
+
+    @Override
+    public V get(K key) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void put(K key, V value) {
+
+    }
+
+    /*
+     * Unsupported Methods:
+     * keySet
+     * remove(key)
+     * remove(key, value)
+     * iterator()
+     */
+
+    @Override
+    public Set<K> keySet() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V remove(K key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V remove(K key, V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        throw new UnsupportedOperationException();
+    }
+}
