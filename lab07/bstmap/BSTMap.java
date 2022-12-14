170968c3a8d4869d64baab390d4f0abdabc2c58e
tomthestrom <thruska29@gmail.com>
2022-12-14 15:52:19 +0100

    BSTMap - BSTnode private class within, private properties root, size, pub method printInOrder() added

--- lab07/bstmap/BSTMap.java
+++ lab07/bstmap/BSTMap.java
@@ -4,6 +4,43 @@ import java.util.Iterator;
 import java.util.Set;
 
 public class BSTMap<K, V> implements Map61B<K, V> {
+    /**
+     * The root of the tree
+     */
+    private BSTNode<K, V> root;
+
+    /**
+     * The nr of nodes in the tree
+     */
+    private int size;
+
+    /**
+     * Represents a node within the BST
+     */
+    private static class BSTNode<K, V> {
+        /**
+         * The key of the node
+         */
+        K key;
+        /**
+         * The value of the node
+         */
+        V value;
+
+        /**
+         * Reference to the left child
+         */
+        BSTNode<K, V> left;
+        /**
+         * Reference to the right child
+         */
+        BSTNode<K, V> right;
+
+        public BSTNode(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
     @Override
     public void clear() {
 
@@ -21,11 +58,18 @@ public class BSTMap<K, V> implements Map61B<K, V> {
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public void put(K key, V value) {
+
+    }
+
+    /**
+     * Prints out the BSTMap in order of increasing Key
+     */
+    public void printInOrder() {
 
     }
 
