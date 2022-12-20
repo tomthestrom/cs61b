e755e6000c7d9bd1cd60116e4e045e9a1aeadd39
tomthestrom <thruska29@gmail.com>
2022-12-20 23:12:25 +0100

    /proj2a/ngordnet/ngrams/TimeSeries.java implemented acc to spec and tested

--- proj2a/ngordnet/ngrams/TimeSeries.java
+++ proj2a/ngordnet/ngrams/TimeSeries.java
@@ -16,24 +16,61 @@ public class TimeSeries extends TreeMap<Integer, Double> {
      *  inclusive of both end points. */
     public TimeSeries(TimeSeries ts, int startYear, int endYear) {
         super();
+        int i = startYear;
+
+        while (i <= endYear) {
+            if (ts.containsKey(i)) {
+                this.put(i, ts.get(i));
+            }
+            i++;
+        }
     }
 
     /** Returns all years for this TimeSeries (in any order). */
     public List<Integer> years() {
-        return null;
+        List<Integer> allYears = new ArrayList<Integer>();
+
+        for(Map.Entry<Integer,Double> entry : this.entrySet()) {
+            Integer key = entry.getKey();
+            allYears.add(key);
+        }
+
+        return allYears;
     }
 
     /** Returns all data for this TimeSeries (in any order).
      *  Must be in the same order as years(). */
     public List<Double> data() {
-        return null;
+        List<Double> allData = new ArrayList<Double>();
+
+        for(Map.Entry<Integer,Double> entry : this.entrySet()) {
+            Double value = entry.getValue();
+            allData.add(value);
+        }
+
+        return allData;
     }
 
     /** Returns the yearwise sum of this TimeSeries with the given TS. In other words, for
      *  each year, sum the data from this TimeSeries with the data from TS. Should return a
      *  new TimeSeries (does not modify this TimeSeries). */
     public TimeSeries plus(TimeSeries ts) {
-        return null;
+       TimeSeries summedSeries = new TimeSeries();
+       summedSeries.putAll(this);
+
+        for(Map.Entry<Integer,Double> entry : ts.entrySet()) {
+            Integer key = entry.getKey();
+            Double value = entry.getValue();
+
+            //if key exists, sum the value in summedSeries(copy of this) with the value in ts;
+            if (summedSeries.containsKey(key)) {
+                value = summedSeries.get(key) + value;
+            }
+
+            summedSeries.put(key, value);
+        }
+
+        return summedSeries;
     }
 
      /** Returns the quotient of the value for each year this TimeSeries divided by the
@@ -41,6 +78,20 @@ public class TimeSeries extends TreeMap<Integer, Double> {
       *  throw an IllegalArgumentException. If TS has a year that is not in this TimeSeries, ignore it.
       *  Should return a new TimeSeries (does not modify this TimeSeries). */
      public TimeSeries dividedBy(TimeSeries ts) {
-        return null;
+         TimeSeries dividedSeries = new TimeSeries();
+         dividedSeries.putAll(this);
+
+         for(Map.Entry<Integer,Double> entry : dividedSeries.entrySet()) {
+             Integer key = entry.getKey();
+
+             if (!ts.containsKey(key)) {
+                 throw new IllegalArgumentException();
+             }
+             Double quotient = dividedSeries.get(key) / ts.get(key);
+
+             dividedSeries.put(key, quotient);
+         }
+
+         return dividedSeries;
     }
 }
