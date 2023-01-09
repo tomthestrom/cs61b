fe0659760b8f810391b88c02ba5db2df39c8ba0b
tomthestrom <thruska29@gmail.com>
2023-01-09 01:11:05 +0100

    proj2b IdToIdsMap - lookup of parentId to a set of childIds associated with it, Graph private attr idtoIdsMap, pub methods: addNode, getNode

--- proj2b/ngordnet/main/Graph.java
+++ proj2b/ngordnet/main/Graph.java
@@ -1,7 +1,30 @@
 package ngordnet.main;
 
-public class Graph {
-    public Graph() {
+import java.util.HashSet;
 
+public class Graph {
+
+    private IdToIdsMap<Integer, HashSet<Integer>> idToIdsMap;
+
+    public Graph() {
+        idToIdsMap = new IdToIdsMap<>();
+    }
+
+    public void addNode(int parentId, int childId) {
+        HashSet<Integer> childIdSet;
+
+        if (idToIdsMap.containsKey(parentId)) {
+            childIdSet = idToIdsMap.get(parentId);
+            childIdSet.add(childId);
+            return;
+        }
+
+        childIdSet = new HashSet<>();
+        childIdSet.add(childId);
+        idToIdsMap.put(parentId, childIdSet);
+    }
+
+    public HashSet<Integer> getNode(int parentId) {
+        return idToIdsMap.get(parentId);
     }
 }
---
+++ proj2b/ngordnet/main/IdToIdsMap.java
@@ -0,0 +1,10 @@
+package ngordnet.main;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class IdToIdsMap<Integer, HashSet> extends HashMap<Integer, HashSet> {
+    public IdToIdsMap() {
+        super();
+    }
+}
