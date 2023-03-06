dfed2f05da8b249d84536d6c3b1be50e844169fc
tomthestrom <thruska29@gmail.com>
2023-03-06 13:11:17 +0100

    proj3/TestGridMathUtils - testCoordsTo1D

--- proj3/byow/Test/TestGridMathUtils.java
+++ proj3/byow/Test/TestGridMathUtils.java
@@ -15,4 +15,23 @@ public class TestGridMathUtils {
 
         assertEquals(expected, GridMathUtils.euclideanDistance(coords1, coords2), epsilon);
     }
+
+    @Test
+    public void testCoordsTo1D() {
+        GridCoords coords1 = new GridCoords(5, 10);
+        GridCoords coords2 = new GridCoords(30, 14);
+        GridCoords coords3 = new GridCoords(0, 0);
+
+        int gridWidth = 50;
+
+        int expected1 = 505;
+        int expected2 = 730;
+        int expected3 = 0;
+
+
+        assertEquals(expected1, GridMathUtils.coordsTo1D(coords1, gridWidth));
+        assertEquals(expected2, GridMathUtils.coordsTo1D(coords2, gridWidth));
+        assertEquals(expected3, GridMathUtils.coordsTo1D(coords3, gridWidth));
+
+    }
 }
