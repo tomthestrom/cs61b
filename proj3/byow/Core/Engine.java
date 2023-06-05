a15a093953811ca5464369c48f8957f938d0a0ee
tomthestrom <thruska29@gmail.com>
2023-06-05 20:56:29 +0200

    proj3/Engine - interactWithInputString - Phase1 - implementation with regex to extract the seed provided in main

--- proj3/byow/Core/Engine.java
+++ proj3/byow/Core/Engine.java
@@ -3,6 +3,7 @@ package byow.Core;
 import byow.TileEngine.TERenderer;
 import byow.TileEngine.TETile;
 import byow.TileEngine.Tileset;
+import java.util.regex.*;
 import org.w3c.dom.Node;
 
 import java.util.List;
@@ -45,7 +46,7 @@ public class Engine {
      * @param input the input string to feed to your program
      * @return the 2D TETile[][] representing the state of the world
      */
-    public static TETile[][] interactWithInputString(String input) {
+    public TETile[][] interactWithInputString(String input) {
         // TODO: Fill out this method so that it run the engine using the input
         // passed in as an argument, and return a 2D tile representation of the
         // world that would have been drawn if the same inputs had been given
@@ -54,12 +55,29 @@ public class Engine {
         // See proj3.byow.InputDemo for a demo of how you can make a nice clean interface
         // that works for many different input types.
 
+        int seed = 1;
+
+        try {
+            //TODO: temp solution for running Phase 1.
+            String inputRegex = "N(\\d+)S";
+            Pattern pattern = Pattern.compile(inputRegex);
+            Matcher matcher = pattern.matcher(input);
+            if (matcher.find()) {
+                String numberString = matcher.group(1);
+                seed = Integer.parseInt(numberString);
+            }
+
+        } catch (Error e) {
+           System.out.println(e);
+        }
+
         TETile[][] finalWorldFrame = new TETile[WIDTH][HEIGHT];
+
         //initially fill the world with NOTHING tiles
         GridDrawer.fillRectangle(finalWorldFrame, Tileset.NOTHING, 0, WIDTH, 0, HEIGHT);
 
         //the seed based on which the pseudorandom map is generated
-        BSPTree worldTree = new BSPTree(WIDTH, HEIGHT, 1234);
+        BSPTree worldTree = new BSPTree(WIDTH, HEIGHT, seed);
 
         List<Room> roomList = worldTree.generateTree().getRoomsFromLeaves();
 
@@ -74,9 +92,8 @@ public class Engine {
     }
 
     public static void main(String[] args) {
-        TERenderer ter = new TERenderer();
-        ter.initialize(WIDTH, HEIGHT);
-
-        ter.renderFrame(interactWithInputString("k"));
+        Engine eng = new Engine();
+        eng.ter.initialize(WIDTH, HEIGHT);
+        eng.ter.renderFrame(eng.interactWithInputString("N23S"));
     }
 }
